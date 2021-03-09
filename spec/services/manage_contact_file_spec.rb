40c53abce9880a7274190bf0cb55dfc8e2c39a9d
Santiago Perez <santiagoperez@Santiagos-MacBook-Pro.local>
2021-03-09 17:59:14 -0500

    Refactor manage_contact_file_spec

--- spec/services/manage_contact_file_spec.rb
+++ spec/services/manage_contact_file_spec.rb
@@ -3,21 +3,29 @@ require 'rails_helper'
 RSpec.describe ManageContactFile do
   let(:user) { create(:user) }
 
-  context 'call' do
-    it 'upload csv' do
-      filename = 'contacts.csv'
-      file_path = "spec/fixtures/#{filename}"
-      result = ::ManageContactFile.new({ file_path: file_path, user: user, filename: filename }).call
-      expect(result.success?).to eq(true)
-      expect(ContactFile.count).to eq(1)
-    end
+  describe '#call' do
+    context 'upload a csv file' do
+      let(:filename) { filename }
+      let(:file_path) { "spec/fixtures/#{filename}" }
+      let(:result) { ::ManageContactFile.new({ file_path: file_path, user: user, filename: filename }).call }
 
-    it 'non existent csv file' do
-      filename = 'non_existent.csv'
-      file_path = "spec/fixtures/#{filename}"
-      result = ::ManageContactFile.new({ file_path: file_path, user: user, filename: filename }).call
-      expect(result.success?).to eq(false)
-      expect(ContactFile.count).to eq(0)
+      context 'when the csv filename is correct' do
+        let(:filename) { 'contacts.csv' }
+
+        it 'a contact file is created' do
+          expect(result.success?).to eq(true)
+          expect(ContactFile.count).to eq(1)
+        end
+      end
+
+      context 'when the csv filename is incorrect' do
+        let(:filename) { 'non_existent.csv' }
+
+        it 'no contact file is created' do
+          expect(result.success?).to eq(false)
+          expect(ContactFile.count).to eq(0)
+        end
+      end
     end
   end
 end

399be5cd349765641ab34385af0abc2d6b75c97b
Santiago Perez <santiagoperez@Santiagos-MacBook-Pro.local>
2021-03-09 18:46:08 -0500

    Refactor manage_contacts_csv_spec

--- spec/services/manage_contacts_csv_spec.rb
+++ spec/services/manage_contacts_csv_spec.rb
@@ -4,76 +4,67 @@ RSpec.describe ManageContactsCsv do
   let(:user) { create(:user) }
   let(:contact_file) { create(:contact_file, user_id: user.id) }
 
-  context 'call' do
-    it 'upload csv - finished with contacts and failed contacts' do
-      filename = 'contacts.csv'
-      file_path = "spec/fixtures/#{filename}"
-      file_content = normalize_csv_file(file_path)
+  describe '#call' do
+    context 'process a csv file' do
+      let(:headers) { headers }
+      let(:filename) { 'contacts.csv' }
+      let(:file_path) { "spec/fixtures/#{filename}" }
+      let(:file_content) { normalize_csv_file(file_path) }
+      let(:result) { ::ManageContactsCsv.new({ headers: headers, contact_file: contact_file }).call }
+      let(:contacts_count) { Contact.where(contact_file: contact_file).count }
+      let(:failed_contacts_count) { FailedContact.where(contact_file: contact_file).count }
 
-      contact_file.csv_file.attach({ io: StringIO.new(file_content), filename: filename })
+      context 'when headers match and filename is correct' do
+        let(:headers) { { email: 'email', name: 'nombre', birth_date: 'fecha_de_nacimiento', phone_number: 'telefono', address: 'direccion', credit_card: 'tarjeta_de_credito' } }
 
-      headers = {
-        email: 'email',
-        name: 'nombre',
-        birth_date: 'fecha_de_nacimiento',
-        phone_number: 'telefono',
-        address: 'direccion',
-        credit_card: 'tarjeta_de_credito'
-      }
+        it "the contact file status is 'finished' and 2 contacts and 2 failed contacts were created" do
+          contact_file.csv_file.attach({ io: StringIO.new(file_content), filename: filename })
 
-      result = ::ManageContactsCsv.new({ headers: headers, contact_file: contact_file }).call
-      expect(result.success?).to eq(true)
-      expect(contact_file.status).to eq('finished')
-      expect(Contact.where(contact_file: contact_file).count).to eq(2)
-      expect(FailedContact.where(contact_file: contact_file).count).to eq(2)
-    end
+          expect(result.success?).to eq(true)
+          expect(contact_file.status).to eq('finished')
+          expect(contacts_count).to eq(2)
+          expect(failed_contacts_count).to eq(2)
+        end
+      end
 
-    it 'upload csv - failed without contacts neither failed contacts' do
-      filename = 'contacts.csv'
-      file_path = "spec/fixtures/#{filename}"
-      file_content = normalize_csv_file(file_path)
+      context 'when headers is an invlaid value and the filename is correct' do
+        let(:headers) { ['something_incorrect'] }
 
-      contact_file.csv_file.attach({ io: StringIO.new(file_content), filename: filename })
+        it "the contact file status is 'failed' and neither contacts nor failed contacts were created" do
+          contact_file.csv_file.attach({ io: StringIO.new(file_content), filename: filename })
 
-      headers = :nil
+          expect(result.success?).to eq(false)
+          expect(contact_file.status).to eq('failed')
+          expect(contacts_count).to eq(0)
+          expect(failed_contacts_count).to eq(0)
+        end
+      end
 
-      result = ::ManageContactsCsv.new({ headers: headers, contact_file: contact_file }).call
-      expect(result.success?).to eq(false)
-      expect(contact_file.status).to eq('failed')
-      expect(Contact.where(contact_file: contact_file).count).to eq(0)
-      expect(FailedContact.where(contact_file: contact_file).count).to eq(0)
-    end
+      context "when headers doesn't match but the filename is correct" do
+        let(:headers) { { email: 'email', name: 'name', birth_date: 'birth_date', phone_number: 'phone_number', address: 'address', credit_card: 'credit_card' } }
 
-    it 'upload csv - failed with failed contacts' do
-      filename = 'contacts.csv'
-      file_path = "spec/fixtures/#{filename}"
-      file_content = normalize_csv_file(file_path)
+        it "the contact file status is 'failed' and 4 failed contacts were created" do
+          contact_file.csv_file.attach({ io: StringIO.new(file_content), filename: filename })
 
-      contact_file.csv_file.attach({ io: StringIO.new(file_content), filename: filename })
+          expect(result.success?).to eq(true)
+          expect(contact_file.status).to eq('failed')
+          expect(failed_contacts_count).to eq(4)
+        end
+      end
 
-      headers = {
-        email: 'email',
-        name: 'name',
-        birth_date: 'birth_date',
-        phone_number: 'phone_number',
-        address: 'address',
-        credit_card: 'credit_card'
-      }
+      context 'when a contact file already has been processed' do
+        let(:processed_contact_file) { ContactFile.new(status: :finished) }
+        let(:result) { ::ManageContactsCsv.new({ headers: {}, contact_file: processed_contact_file }).call }
 
-      result = ::ManageContactsCsv.new({ headers: headers, contact_file: contact_file }).call
-      expect(result.success?).to eq(true)
-      expect(contact_file.status).to eq('failed')
-      expect(FailedContact.where(contact_file: contact_file).count).to eq(4)
-    end
+        it 'raise an error message and neither contacts nor failed contacts are created' do
+          processed_contact_file.save(validate: false)
 
-    it 'do not allow process already processed contact files' do
-      processed_contact_file = ContactFile.new(status: :finished)
-      processed_contact_file.save(validate: false)
-
-      result = ::ManageContactsCsv.new({ headers: {}, contact_file: processed_contact_file }).call
-      expect(result.success?).to eq(false)
-      expect(result.payload[:error]).to eq('Contact file has been processed')
-      expect(Contact.where(contact_file_id: processed_contact_file.id).count).to eq(0)
+          expect(result.success?).to eq(false)
+          expect(result.payload[:error]).to eq('Contact file has been processed')
+          expect(Contact.where(contact_file_id: processed_contact_file.id).count).to eq(0)
+          expect(FailedContact.where(contact_file: processed_contact_file).count).to eq(0)
+        end
+      end
     end
   end
 end

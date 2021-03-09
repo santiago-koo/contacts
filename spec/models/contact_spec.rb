8c0e7ab5daa03547de05d82a4f9be5e74bf8486a
Santiago Perez <santiagoperez@Santiagos-MacBook-Pro.local>
2021-03-09 17:05:15 -0500

    Refactor contact_spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -29,72 +29,63 @@ RSpec.describe Contact, type: :model do
     it { should validate_presence_of :birth_date }
     it { should validate_presence_of :address }
     it { should validate_presence_of :credit_card }
+    it { should validate_uniqueness_of(:email).scoped_to(:contact_file_id).with_message('Email already taken') }
+  end
 
-    it 'email format validation' do
-      email = 'testtest.com'
-      new_contact = Contact.new(email: email)
+  context 'custom validations' do
+    let(:email) { 'testtest.com' }
+    let(:phone_number) { '3193206968' }
+    let(:birth_date) { '1995/12/12' }
+    let(:credit_card) { '112124234f' }
+    let(:new_contact) { Contact.new }
+
+    it 'email is invalid with message' do
+      new_contact.email = email
       new_contact.valid?
 
       expect(new_contact.errors.messages[:email].first).to eq('Invalid email')
     end
 
-    it 'phone number format validation' do
-      phone_number = '3193206968'
-      new_contact = Contact.new(phone_number: phone_number)
+    it 'phone number is invalid with message' do
+      new_contact.phone_number = phone_number
       new_contact.valid?
 
       expect(new_contact.errors.messages[:phone_number].first).to eq('Invalid phone number')
     end
 
-    it 'custom email validation' do
-      email = 'test@test.com'
-      new_contact = Contact.new(email: email)
-      new_contact.save(validate: false)
-
-      new_contact2 = Contact.new(email: email)
-      new_contact2.valid?
-
-      expect(new_contact2.errors.messages[:email].first).to eq('Email already taken')
-      expect(Contact.count).to eq(1)
-    end
-
-    it 'custom birth date validation' do
-      birth_date = '1995/12/12'
-      new_contact = Contact.new(birth_date: birth_date)
+    it 'birth date is invalid with message' do
+      new_contact.birth_date = birth_date
       new_contact.valid?
 
       expect(new_contact.errors.messages[:birth_date].first).to eq('Invalid birth date')
     end
 
-    it 'custom credit card validation' do
-      credit_card = '112124234f'
-      new_contact = Contact.new(credit_card: credit_card)
+    it 'credit card is invalid with message' do
+      new_contact.credit_card = credit_card
       new_contact.valid?
 
       expect(new_contact.errors.messages[:credit_card].first).to eq('Invalid credit card')
     end
   end
 
-  context 'callbacks' do
+  context 'when the given contact information is correct' do
     let(:user) { create(:user) }
     let(:contact_file) { create(:contact_file, user_id: user.id) }
-
-    it '' do
-      contact_params = {
+    let(:contact_params) {
+      {
         email: 'test@test.com',
         phone_number: '(+00) 000-000-00-00',
         name: 'Pablo',
         birth_date: '1995-12-12',
         address: 'fake street 123',
         credit_card: '4111111111111111',
-        contact_file: contact_file,
+        contact_file: contact_file
       }
-      new_contact = Contact.new(contact_params)
-      new_contact.save
+    }
 
-      expect(new_contact.franchise).to eq('Visa')
-      expect(new_contact.last_four_credit_card_numbers).to eq(contact_params[:credit_card].last(4))
-      expect(new_contact.credit_card).to eq(Digest::SHA2.hexdigest(contact_params[:credit_card]))
+    it 'new contact is valid' do
+      new_contact = Contact.new(contact_params)
+      expect(new_contact).to be_valid
     end
   end
 end
